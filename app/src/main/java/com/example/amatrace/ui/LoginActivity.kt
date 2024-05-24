1e6bff1ea2b9fa14737b3ec5abcb8ce3ad3cdc7f
Dinda Juliana <dindaj473@gmail.com>
2024-05-24 11:10:58 +0700

    buat xml landingpage & login coba di run

---
+++ app/src/main/java/com/example/amatrace/ui/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.amatrace.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.amatrace.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Tambahkan logika lain yang Anda butuhkan di sini
+    }
+}
