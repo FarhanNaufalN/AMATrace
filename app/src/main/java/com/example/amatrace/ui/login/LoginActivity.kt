8eadd54997967cf2af179b4cb4bb9c48ab1c511f
FarhanNaufalNurdiansyah <farhannaufalnr@gmail.com>
2024-05-26 20:06:05 +0700

    Perbaiki Logo

--- app/src/main/java/com/example/amatrace/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/amatrace/ui/login/LoginActivity.kt
@@ -1,12 +1,14 @@
 package com.example.amatrace.ui.login
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.amatrace.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
     }
