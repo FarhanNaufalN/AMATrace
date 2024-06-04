2b4ce1f25357163f3bf36564b76a68f5477f1f7e
Dinda Juliana <dindaj473@gmail.com>
2024-06-04 21:45:53 +0700

    Menambahkan xml detail batch produk producer

---
+++ app/src/main/java/com/example/amatrace/pages/producer/ui/detail/batchproduk/DetailBatchProducerActivity.kt
@@ -0,0 +1,21 @@
+package com.example.amatrace.pages.producer.ui.detail.batchproduk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.amatrace.R
+
+class DetailBatchProducerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_batch_producer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
