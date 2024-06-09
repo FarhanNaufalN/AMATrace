26b191d53a61728eed1c64703a26a02e4dc408c0
Dinda Juliana <dindaj473@gmail.com>
2024-06-09 20:53:14 +0700

    detail raw produk

---
+++ app/src/main/java/com/example/amatrace/pages/producer/ui/detail/rawProduk/DetailRawProdukActivity.kt
@@ -0,0 +1,21 @@
+package com.example.amatrace.pages.producer.ui.detail.rawProduk
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.amatrace.R
+
+class DetailRawProdukActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail_raw_produk)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
