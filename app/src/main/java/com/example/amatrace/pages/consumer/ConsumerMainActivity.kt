4f5b27833e70fdf4de275a8eb2bc10fb4c942f66
FarhanNaufalNurdiansyah <farhannaufalnr@gmail.com>
2024-06-10 00:22:04 +0700

    Consumer

---
+++ app/src/main/java/com/example/amatrace/pages/consumer/ConsumerMainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.amatrace.pages.consumer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.amatrace.R
+
+class ConsumerMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_consumer_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
