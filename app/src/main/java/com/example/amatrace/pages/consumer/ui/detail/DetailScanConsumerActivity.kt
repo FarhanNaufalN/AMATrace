644f7b480d1b924dc60de283f238f0931ebd32d1
Dinda Juliana <dindaj473@gmail.com>
2024-06-11 01:55:14 +0700

    detailscanconsumer

--- app/src/main/java/com/example/amatrace/pages/consumer/ui/detail/DetailScanConsumerActivity.kt
+++ app/src/main/java/com/example/amatrace/pages/consumer/ui/detail/DetailScanConsumerActivity.kt
@@ -1,21 +1,37 @@
 package com.example.amatrace.pages.consumer.ui.detail
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.amatrace.R
+import com.example.amatrace.databinding.ActivityDetailScanConsumerBinding
 
 class DetailScanConsumerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailScanConsumerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_detail_scan_consumer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityDetailScanConsumerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.detailProductButton.setOnClickListener {
+            toggleVisibility(binding.detailProductCard)
         }
+
+        binding.prosesProduksiButton.setOnClickListener {
+            toggleVisibility(binding.prosesProduksiCard)
+        }
+
+        binding.labelProductButton.setOnClickListener {
+            toggleVisibility(binding.labelProductCard)
+        }
+
+    }
+
+    private fun toggleVisibility(cardView: View) {
+        cardView.visibility = if (cardView.visibility == View.GONE) View.VISIBLE else View.GONE
     }
 }
